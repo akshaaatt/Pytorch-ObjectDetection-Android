5ba1ce3648c5b7208c27baa413381fee2bfdf8fa
Akshat Tiwari <51470769+akshaaatt@users.noreply.github.com>
2023-06-20 19:07:46 +0530

    Code cleanup

--- app/src/main/java/com/limurse/objectdetection/ui/MainActivity.kt
+++ app/src/main/java/com/limurse/objectdetection/ui/MainActivity.kt
@@ -223,12 +223,12 @@ class MainActivity : AppCompatActivity(), Runnable {
 
     companion object {
         @Throws(IOException::class)
-        fun assetFilePath(context: Context, assetName: String?): String {
+        fun assetFilePath(context: Context, assetName: String): String {
             val file = File(context.filesDir, assetName)
             if (file.exists() && file.length() > 0) {
                 return file.absolutePath
             }
-            context.assets.open((assetName)!!).use { `is` ->
+            context.assets.open((assetName)).use { `is` ->
                 FileOutputStream(file).use { os ->
                     val buffer = ByteArray(4 * 1024)
                     var read: Int
